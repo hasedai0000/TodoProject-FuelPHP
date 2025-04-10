783c4f7c7493e057efceab59a562cb6a6bf2eaa1
daichi-hasegawa-styleedge <d.hasegawa@styleedge.co.jp>
2025-04-10 19:26:46 +0900

    DBの追加と設定

--- fuel-app/fuel/app/config/db.php
+++ fuel-app/fuel/app/config/db.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Fuel is a fast, lightweight, community driven PHP 5.4+ framework.
  *
@@ -21,5 +22,32 @@
  */
 
 return array(
+  'active' => 'fuel_db',
 
+  'default' => array(
+    'type' => 'pdo',
+    'connection' => array(
+      'persistent' => false,
+    ),
+    'identifier' => '`',
+    'table_prefix' => '',
+    'charset' => 'utf8',
+    'enable_cache' => true,
+    'profiling' => true,
+  ),
+  // データベース名
+  'fuel_db' => array(
+    'type' => 'pdo',
+    'connection' => array(
+      'hostname' => 'mysql',
+      'database' => 'fuel_db',
+      'username' => 'fuel',
+      'password' => 'fuel_pass',
+      'persistent' => false,
+    ),
+    'table_prefix' => '',
+    'charset' => 'utf8',
+    'enable_cache' => true,
+    'profiling' => true,
+  ),
 );
