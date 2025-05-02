976cd529ba41a35a9c6784c5e8ba15f7cd8cdcd0
daichi-hasegawa-styleedge <d.hasegawa@styleedge.co.jp>
2025-05-02 22:30:52 +0900

    マイページにログイン中のユーザー情報の表示

--- fuel-app/fuel/app/views/member/mypage.php
+++ fuel-app/fuel/app/views/member/mypage.php
@@ -1,3 +1,7 @@
-<div style="text-align: center;">
-  <h1>マイページ</h1>
+<div class="ctn-main">
+  <session class="ctn-form">
+    <h1>マイページ</h1>
+    <p>ユーザー ID：<?= $user['user_id']; ?></p>
+    <p>ユーザー名：<?= $user['user_name']; ?></p>
+  </session>
 </div>
